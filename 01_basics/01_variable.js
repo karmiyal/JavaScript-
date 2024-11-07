41883a9eb7206dea2074805987a63b279653e299
karmiyal <168079328+karmiyal@users.noreply.github.com>
2024-11-07 13:42:00 +0000

    Variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId=114553   //constant variable
+let accountEmail="babita2002karmiyal@gmail.com" //let variable
+var accountPassword="1234" //variable 
+accountCity="Haldwani"
+let accountState;
+//accountId =2  //Not Allowed
+accountEmail="Karmiyalbabita@gmail.com"
+accountPassword="Karmiyal"
+accountCity="Rudrapur"
+console.log(accountId);
+/*
+prefer not to use var
+because of issue in block scope and function scopw
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
