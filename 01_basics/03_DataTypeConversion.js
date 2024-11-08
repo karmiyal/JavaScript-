2de381de63aedee158c9db6936e66797cf91d19d
karmiyal <168079328+karmiyal@users.noreply.github.com>
2024-11-08 05:00:50 +0000

    Operation in dataType

--- 01_basics/03_DataTypeConversion.js
+++ 01_basics/03_DataTypeConversion.js
@@ -29,3 +29,36 @@ console.log(booleanIsLoggedIn);
 let someNumber=33;
 let stringnumber=String(someNumber);
 console.log(typeof stringnumber);
+            /*****************************Operations********************************/
+let value =3;
+let negvalue= -value;
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+                                    /*Adding String*/
+let str1="Babita"
+let str2=" karmiyal"
+let str3= str1+ str2;
+console.log(str3);
+
+                                    /*Adding String with Number*/
+console.log("1"+3);//13
+console.log(1+"3");//13
+console.log("1"+2+3);//123
+console.log(1+2+"3")//33
+
+/* Inappropriate way that we should never follow */
+console.log(+true)//1
+console.log(+"")//0
+let num1,num2,num3;
+num1=num2=num3=2+2;
+
+let gameCounter =100;
+gameCounter++;
+console.log(gameCounter);//101
+
+//Note prefix and postfix operator
