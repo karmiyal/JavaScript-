668d87ecac3a0e3f83f49d095381539f3fd71b9c
karmiyal <168079328+karmiyal@users.noreply.github.com>
2024-11-08 04:06:11 +0000

    DataTypes

---
+++ 01_basics/02_DataType.js
@@ -0,0 +1,16 @@
+"use strict";//treat all JS code as newer version
+//alert("Hello"); //we are using node js not browser
+
+let name1 ="Babita";//string=>""
+let age=19;//Number=> 2^52 //bigint
+let isLoggedIn=true;//Boolean=>(true or false)
+let empty;//Undeined=>no value is assigned
+
+//null =>standalone value ,we can assigned null as a value to any variable 
+//symbole=>unique
+//object
+
+console.log(typeof name1);
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
+
